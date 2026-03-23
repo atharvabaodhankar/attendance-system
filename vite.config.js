e40174c8981c23f07922756b36f74b1fb79c0179
Atharva Baodhankar <108174720+atharvabaodhankar@users.noreply.github.com>
2026-03-23 11:47:17 +0530

    feat: Add teacher session management page with QR code attendance and location verification, and update Vite host configuration.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true // set to true to listen on all addresses, including LAN and public addresses
+    // You can also use '0.0.0.0' explicitly if 'true' doesn't work in some environments
+  },
   plugins: [tailwindcss(),react()],
 })
